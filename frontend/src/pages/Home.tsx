c5aaf17c623d83616e103827785cb6c2740f85f5
dardenkyle <darden_kyle@hotmail.com>
2025-08-29 16:18:22 -0500

    feat: integrate Tailwind CSS and update styling
    - Added Tailwind CSS and its dependencies to the project.
    - Removed App.css and migrated styles to Tailwind utility classes.
    - Updated index.css to include Tailwind directives and custom font.
    - Created postcss.config.js and tailwind.config.js for Tailwind setup.

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,6 +4,12 @@ export function Home() {
   return (
     <section>
       <h1>Home</h1>
+      <h2 className="text-3xl font-bold text-emerald-600">
+        Tailwind is working
+      </h2>
+      <div className="p-6">
+        <h1 className="text-3xl font-bold text-blue-600">Tailwind OK</h1>
+      </div>
       <p>Welcome to your new Vite + React + TS app with routing.</p>
     </section>
   );
